2b4647aefcda61b9dfab0d3f0018e67e51835824
Gijs Molenaar <gijs@pythonic.nl>
2016-08-22 12:34:18 +0200

    also install config file

--- src/tparin.f
+++ src/tparin.f
@@ -94,7 +94,7 @@ c     default values of parameters
 
       call getenv ('TEMPO',path)
       lpth = index(path,' ')-1
-      hlpfile = '/usr/share/tempo/tempo.hlp'	
+      hlpfile = path(1:lpth)//'/tempo.hlp'	
 
 
       narg = iargc()
